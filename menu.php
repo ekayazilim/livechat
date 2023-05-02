2c641ae10f8d6ec032abea6c78f32f221c3ecdb8
Eka Yazilim <132365560+ekayazilim@users.noreply.github.com>
2023-05-02 16:41:32 +0300

    Create menu.php
    Bu kodlar, Bootstrap kullanarak bir navigasyon çubuğu ve arama alanını içerir. Menü öğeleri gerektiği gibi düzenlenebilir veya yeni öğeler eklenebilir. Bu dosyayı diğer sayfalarda dahil ederek, site menünüzü kolayca gösterebilirsiniz.

---
+++ menu.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Eka Chat</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Anasayfa <span class="sr-only">(Mevcut)</span></a>
+            </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="search" placeholder="Ara" aria-label="Ara">
+            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Ara</button>
+        </form>
+    </div>
+</nav>
