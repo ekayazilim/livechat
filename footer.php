0e30bea051705663b9c91177c595610d124616fe
Eka Yazilim <132365560+ekayazilim@users.noreply.github.com>
2023-05-02 16:39:56 +0300

    Create footer.php
    Bu kodlar, Bootstrap kullanarak bir altbilgi bölümü oluşturur. <?php echo date("Y"); ?> kodu, yılın otomatik olarak güncellenmesini sağlar. İsterseniz alt bilgi kısmındaki diğer öğeleri de düzenleyebilirsiniz.
    
    Bu dosyayı diğer sayfalarda dahil ederek, site altbilgisini kolayca gösterebilirsiniz.

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-light py-3 mt-3">
+    <div class="container text-center">
+        <p>Tüm hakları saklıdır &copy; <?php echo date("Y"); ?> Eka Yazılım ve Bilişim Sistemleri</p>
+    </div>
+</footer>
